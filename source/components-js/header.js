1b3c41bc222597478b8f14d0cf90c1e6fe5a9f3f
n7wt <salanamir0@gmail.com>
2023-05-20 00:53:28 +0300

    Add new page "404 error - history"

--- source/components-js/header.js
+++ source/components-js/header.js
@@ -1,35 +1,31 @@
-const langToggle = document.getElementById('language-toggle');
-const menuChange = document.querySelectorAll('.menu__link');
+document.addEventListener("DOMContentLoaded", function () {
+  const langToggle = document.querySelector(".language-switch");
+  const menuChange = document.querySelectorAll(".menu__link");
 
-langToggle.addEventListener('click', () => {
-  langToggle.classList.toggle('toggle');
-  if (langToggle.classList.contains('toggle')) {
-    menuChange.forEach(link => {
-      if (link.textContent === 'Главная') {
-        link.textContent = 'Бохь';
-      } else if (link.textContent === 'История') {
-        link.textContent = 'Истори';
-      } else if (link.textContent === 'Культура') {
-        link.textContent = 'Оьздангалла';
-      } else if (link.textContent === 'Галерея') {
-        link.textContent = 'Галерея';
-      } else if (link.textContent === 'Контакты') {
-        link.textContent = 'Контактш';
+  langToggle.addEventListener("click", function () {
+    langToggle.classList.toggle("toggle");
+    menuChange.forEach(function (link) {
+      if (link.textContent === "Главная") {
+        link.textContent = "Бохь";
+      } else if (link.textContent === "История") {
+        link.textContent = "Истори";
+      } else if (link.textContent === "Культура") {
+        link.textContent = "Оьздангалла";
+      } else if (link.textContent === "Галерея") {
+        link.textContent = "Галерея";
+      } else if (link.textContent === "Контакты") {
+        link.textContent = "Контактш";
+      } else if (link.textContent === "Бохь") {
+        link.textContent = "Главная";
+      } else if (link.textContent === "Истори") {
+        link.textContent = "История";
+      } else if (link.textContent === "Оьздангалла") {
+        link.textContent = "Культура";
+      } else if (link.textContent === "Галерея") {
+        link.textContent = "Галерея";
+      } else if (link.textContent === "Контактш") {
+        link.textContent = "Контакты";
       }
     });
-  } else {
-    menuChange.forEach(link => {
-      if (link.textContent === 'Бохь') {
-        link.textContent = 'Главная';
-      } else if (link.textContent === 'Истори') {
-        link.textContent = 'История';
-      } else if (link.textContent === 'Оьздангалла') {
-        link.textContent = 'Культура';
-      } else if (link.textContent === 'Галерея') {
-        link.textContent = 'Галерея';
-      } else if (link.textContent === 'Контактш') {
-        link.textContent = 'Контакты';
-      }
-    });
-  }
+  });
 });
